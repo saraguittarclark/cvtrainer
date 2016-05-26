d4cec288a630738e1fdac601a610d0e8942785ec
Sara Clark <sara.guittar@gmail.com>
2016-05-25 22:01:54 -0400

    almost done, need to add media queries and reformat displayed data

--- dist/scripts/controllers/CVTrainerCtrl.js
+++ dist/scripts/controllers/CVTrainerCtrl.js
@@ -13,38 +13,9 @@
 		$scope.decrement = function () {
 			$scope.currentIndex--;
 		};
-
-
-
-
-/*
-
-		$scope.clicked = false;
-		var numClicks = 0;
-
-		$scope.clickEvent = function() {
-			this.clicked = true;
-			numClicks++;
-			if (numClicks > 1) {
-				$scope.clicked = true;
-			};
-
-		};*/
-		//when reload is clicked, no option to see again
 	}
 
 	angular
 		.module('cvapp')
 		.controller('CVTrainerCtrl', ['aiData', '$scope', CVTrainerCtrl]);
 })();
-
-
-
-//when the next li is clicked, completely remove from DOM
-//add variable "done" and can't go back after 5 have been clicked
-//when clicked = 5, hide all
-
-//media queries!
-//portfolio stuff
-//dumb self-reflection
-//animate hide/show
